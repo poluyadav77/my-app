e98b57f5cd65e6a7510751ee71aa530fe71bec50
poluyadav77 <poluyadav77@gmail.com>
2026-01-31 12:36:43 +0530

    changes from my feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi This is polu
+          Hi This is polu #pushing from polu branch
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 111
         </a>
       </header>
     </div>
